0628d5401e2d550cead2175b0906b1526ab629d4
Taha Elleuch <tahaeleuch@gmail.com>
2020-08-03 21:52:27 +0000

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
 # Using puppet create a file
 
 file { '/tmp/holberton':
-  ensure  => '/tmp/holberto',
-  path    => '/tmp/holberton",
+  path    => '/tmp/holberton',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
