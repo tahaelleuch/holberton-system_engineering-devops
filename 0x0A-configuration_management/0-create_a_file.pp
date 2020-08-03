8432f7db7d10a87f01be97559e1974b459727548
Taha Elleuch <tahaeleuch@gmail.com>
2020-08-03 21:41:51 +0000

    0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Using puppet create a file
+
+file { '/tmp/holberton':
+  ensure  => '/tmp/holberto',
+  path    => '/tmp/holberton",
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
