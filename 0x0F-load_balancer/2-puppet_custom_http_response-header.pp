abd7705c96ef2d00be06301e7289ecc2da030a6c
Taha Elleuch <tahaeleuch@gmail.com>
2020-08-16 10:41:42 +0000

    2-puppet_custom_http_response-header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -5,6 +5,6 @@ exec {'http header':
 command  => 'sudo apt-get update
 sudo apt-get -y install nginx
 sudo sed -i "11i\\\tadd_header X-Served-By $HOSTNAME;" /etc/nginx/nginx.conf
-sudo service nginx start'
+sudo service nginx start',
 provider => shell,
 }
