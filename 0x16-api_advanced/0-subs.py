9459f731e2cebdc626ae4a37a11ca7a8ed7977ce
Taha Elleuch <Tahaeleuch@gmail.com>
2020-09-17 12:16:29 +0100

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,15 +2,14 @@
 """get number of follwers with reddit api"""
 
 import requests
-from fake_useragent import UserAgent
+
 
 def number_of_subscribers(subreddit):
     """get the numbers"""
-    ua = UserAgent()
+    ua = 'Mozilla/5.0 (X11; Ubuntu; Linux i686) Gecko/20100101 Firefox/15'
     about = requests.get("https://www.reddit.com/r/" +
-                        subreddit + "/about.json",
-                         headers={'User-Agent': ua.random}).json()
-
+                         subreddit + "/about.json",
+                         headers={'User-Agent': ua}).json()
     try:
         data = about["data"]
         return data["subscribers"]
