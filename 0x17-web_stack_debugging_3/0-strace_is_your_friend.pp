fae42bca2dcca27f45f56b5cfcb692af52fd1d1d
Taha elleuch <tahaeleuch@gmail.com>
2020-09-30 21:13:22 +0000

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Using Puppet, fixapache
 
 exec { 'fix':
-  command  => 'sed -i 's/phpp/php/' /var/www/html/wp-settings.php',
+  command  => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
   provider => 'shell',
 }
