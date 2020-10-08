6b95e42e00a48db7a2b4bd03359b036466df1e95
Taha elleuch <tahaeleuch@gmail.com>
2020-10-08 20:35:24 +0000

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # Using Puppet, fix nginx ulimiz
 
 exec { 'fix':
-  command  => "sudo sed -i 's/15/4096/g' /etc/default/nginx
+command  => 'sudo sed -i 's/15/4096/g' /etc/default/nginx
 service nginx restart',
-  provider => 'shell',
+provider => 'shell',
 }
